59bd508784424269273cf7879b019b93b7dbcc23
katussska <katkabaier@gmail.com>
2024-12-18 00:00:44 +0100

    FE-registration, login, profile details

--- frontend/src/components/profile/ProfileDetails.tsx
+++ frontend/src/components/profile/ProfileDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function ProfileDetails() {
   const [user, setUser] = useState({ name: '', email: '', phone: '', registrationDate: '' });
